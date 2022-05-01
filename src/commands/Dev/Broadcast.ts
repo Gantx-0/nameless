de0d48e4653d1ba3386c7e5d9e1dd3fddb64e4a9
Lakshsharma31 <100475528+Lakshsharma31@users.noreply.github.com>
2022-05-01 17:05:13 +0530

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -29,8 +29,8 @@ export default class Command extends BaseCommand {
 			return void (await M.reply(`Please provide the Broadcast Message.`));
 		const term = joined.trim();
 		const images = [
-			"https://wallpapercave.com/uwp/uwp1997565.jpeg",
-			"https://wallpapercave.com/uwpt/uwp1997563.jpeg",
+			"https://telegra.ph/file/1ae03ed52ea67638a4d2c.jpg",
+			"https://telegra.ph/file/1ae03ed52ea67638a4d2c.jpg",
 		];
 		const selected = images[Math.floor(Math.random() * images.length)];
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -41,7 +41,7 @@ export default class Command extends BaseCommand {
 			.map((jids) => (jids.includes("g.us") ? jids : null))
 			.filter((v) => v);
 		for (let i = 0; i < chats.length; i++) {
-			const text = `🎆_*BEYOND*_🎆\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
+			const text = `𝕜𝕒𝕜𝕒𝕤𝕙𝕚 𝕓𝕠𝕥𝕫.𝕚𝕟𝕔\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
 			this.client.sendMessage(chats[i], { url: selected }, MessageType.image, {
 				caption: `${text}`,
 				contextInfo: {
